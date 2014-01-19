e4aa8da5cdf0be7edff34827db7056043448e046
Jonas Wielicki <j.wielicki@sotecware.net>
2014-01-19 18:01:20 +0100

    Implement case folded dict

--- teapot/mime.py
+++ teapot/mime.py
@@ -1,5 +1,6 @@
 import copy
 import codecs
+import itertools
 
 class Type:
     def __init__(self, type_, subtype,
@@ -53,3 +54,70 @@ class Type:
             repr(self.__parameters))
 
 Type.text_plain = Type("text", "plain")
+
+class CaseFoldedDict(dict):
+    class __undefined:
+        pass
+
+    _transform_key = staticmethod(str.casefold)
+
+    @classmethod
+    def _transform_item(cls, item):
+        return (cls._transform_key(item[0]), item[1])
+
+    @classmethod
+    def _transform_items_iterable(cls, iterable):
+        return map(cls._transform_item, iterable)
+
+    @classmethod
+    def _items_iterable_from_mapping_or_iterable(
+            cls, mapping_or_iterable):
+        try:
+            items_iterable = mapping_or_iterable.items()
+        except AttributeError:
+            items_iterable = mapping_or_iterable
+        return cls._transform_items_iterable(items_iterable)
+
+    def __init__(self, mapping_or_iterable=__undefined, **kwargs):
+        if mapping_or_iterable is not self.__undefined:
+            items_iterable = self._items_iterable_from_mapping_or_iterable(
+                mapping_or_iterable)
+        else:
+            items_iterable = None
+
+        if kwargs:
+            if items_iterable is not None:
+                items_iterable = itertools.chain(
+                    items_iterable,
+                    self._transform_items_iterable(kwargs.items()))
+            else:
+                items_iterable = self._transform_items_iterable(kwargs.items())
+
+        if items_iterable is not None:
+            super().__init__(items_iterable)
+        else:
+            super().__init__()
+
+    def __delitem__(self, key):
+        return super().__delitem__(self._transform_key(key))
+
+    def __getitem__(self, key):
+        return super().__getitem__(self._transform_key(key))
+
+    def __setitem__(self, key, value):
+        return super().__setitem__(self._transform_key(key), value)
+
+    def get(self, key, *args, **kwargs):
+        return super().get(self._transform_key(key), *args, **kwargs)
+
+    @classmethod
+    def fromkeys(cls, sequence, *args, **kwargs):
+        return super().fromkeys(
+            map(cls._transform_key, sequence), *args, **kwargs)
+
+    def pop(self, key, *args, **kwargs):
+        return super().pop(self._transform_key(key), *args, **kwargs)
+
+    def update(self, other):
+        super().update(
+            self._items_iterable_from_mapping_or_iterable(other))
--- tests/mime.py
+++ tests/mime.py
@@ -23,3 +23,54 @@ class TestType(unittest.TestCase):
                           "text",
                           "plain",
                           charset=self.UNKNOWN_CODEC)
+
+class TestCaseFoldedDict(unittest.TestCase):
+    def test_construct_from_dict(self):
+        d = {"A": "a", "B": "b"}
+        cfd = teapot.mime.CaseFoldedDict(d)
+        self.assertEqual(cfd["a"], d["A"])
+        self.assertEqual(cfd["A"], d["A"])
+        self.assertEqual(cfd["b"], d["B"])
+        self.assertEqual(cfd["B"], d["B"])
+
+    def test_construct_from_sequence(self):
+        seq = [("A", "1"), ("a", "2"), ("B", "3")]
+        cfd = teapot.mime.CaseFoldedDict(seq)
+        self.assertEqual(cfd["a"], "2")
+        self.assertEqual(cfd["A"], "2")
+        self.assertEqual(cfd["B"], "3")
+
+    def test_construct_with_kwargs(self):
+        seq = [("A", "1"), ("a", "2"), ("B", "3")]
+        cfd = teapot.mime.CaseFoldedDict(seq, B="4")
+        self.assertEqual(cfd["a"], "2")
+        self.assertEqual(cfd["A"], "2")
+        self.assertEqual(cfd["B"], "4")
+
+    def test_item_casefolding(self):
+        cfd = teapot.mime.CaseFoldedDict()
+        cfd["A"] = "Foo"
+        self.assertEqual(cfd["a"], "Foo")
+        self.assertEqual(cfd["A"], "Foo")
+
+    def test_update_casefolding(self):
+        cfd = teapot.mime.CaseFoldedDict()
+        d = {"A": "1"}
+        cfd["a"] = "2"
+        cfd.update(d)
+        self.assertEqual(cfd["a"], "1")
+
+    def test_pop_casefolding(self):
+        cfd = teapot.mime.CaseFoldedDict()
+        cfd["a"] = "2"
+        self.assertEqual(cfd.pop("A"), "2")
+        self.assertRaises(KeyError, cfd.pop, "a")
+
+    def test_fromkeys_casefolding(self):
+        cfd = teapot.mime.CaseFoldedDict.fromkeys(["A", "B", "C", "a"])
+        self.assertEqual(len(cfd), 3)
+
+    def test_get_casefolding(self):
+        cfd = teapot.mime.CaseFoldedDict()
+        cfd["a"] = "2"
+        self.assertEqual(cfd.get("A"), "2")
