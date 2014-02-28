07a56ddeedc4abf4e69dde9e665272ae2d66b176
Jonas Wielicki <j.wielicki@sotecware.net>
2014-02-28 16:49:27 +0100

    Add SessionMixin, which creates a sqlalchemy session per request

---
+++ teapot/sqlalchemy.py
@@ -0,0 +1,28 @@
+"""
+SQLAlchemy Utilities
+####################
+
+Teapot provides some utilities to better integrate with the popular
+:mod:`sqlalchemy` database package.
+
+Session management
+==================
+
+There is a :class:`~teapot.routing.Router` mixin which provides a ``dbsession``
+attribute on the :class:`~teapot.request.Request` instance. For this it uses a
+standard :class:`sqlalchemy.orm.session.sessionmaker`.
+
+"""
+
+class SessionMixin:
+    def __init__(self, sessionmaker, **kwargs):
+        super().__init__(**kwargs)
+        self._sessionmaker = sessionmaker
+
+    def pre_route_hook(self, request):
+        super().pre_route_hook(request)
+        request.dbsession = self._sessionmaker()
+
+    def post_response_cleanup(self, request):
+        del request.dbsession
+        super().post_response_cleanup(request)
