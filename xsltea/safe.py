f0fa7df5b392a49a96e84698a55084e45ec93ecb
Jonas Wielicki <j.wielicki@sotecware.net>
2014-03-02 15:27:58 +0100

    Introduce several safety levels

--- xsltea/safe.py
+++ xsltea/safe.py
@@ -10,8 +10,15 @@ called.
 
 .. autoclass:: IncludeProcessor
 
+To specify the safety level used for template evaluation of the different
+available processors, the values of the :class:`SafetyLevel` enum can be used:
+
+.. autoclass:: SafetyLevel
+
 """
+import abc
 import ast
+import collections
 import functools
 import logging
 
@@ -23,6 +30,201 @@ from .processor import TemplateProcessor
 from .namespaces import shared_ns
 
 logger = logging.getLogger(__name__)
+
+@functools.total_ordering
+class SafetyLevel:
+    """
+    Enum for the safety level of template evaluation. Three different safety
+    levels exist:
+
+    .. attribute:: conservative
+
+    .. attribute:: experimental
+
+    .. attribute:: unsafe
+    """
+
+    safety = None
+
+    def __lt__(self, other):
+        return self.safety < other.safety
+
+    def __le__(self, other):
+        return self.safety <= other.safety
+
+    def __eq__(self, other):
+        return type(self) == type(other)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    @abc.abstractmethod
+    def _check_safety(self, ast):
+        """
+        Check whether the given *ast* object is safe. If it is safe,
+        :data:`True` must be returned. If it is unsafe, the method must either
+        return :data:`False` or raise a :class:`ValueError` explaining why it is
+        not safe.
+
+        Subclasses must implement this.
+
+        **Do not call this directly**, but instead use :meth:`check_safety`.
+        """
+
+    def check_safety(self, ast):
+        """
+        Check the given *ast* for safety. Raises :class:`ValueError` if it is
+        not considered safe.
+        """
+
+        if not self._check_safety(ast):
+            raise ValueError("The code passed is not allowed in the current"
+                             " safety restrictions ({!s})".format(self))
+
+    def check_code_safety(self, src, mode="exec"):
+        nodes = compile(src, "", mode, ast.PyCF_ONLY_AST).body
+        if mode == "exec":
+            for item in nodes:
+                self.check_safety(item)
+        else:
+            self.check_safety(nodes)
+
+    def compile_ast(self, ast, filename, mode):
+        """
+        Checks if the given *ast* object is safe with respect to the safety
+        constraints defined by the subclass. If it is safed, it is passed
+        together with the other arguments to the python :func:`compile`
+        builtin.
+
+        For a definition of *safe* with respect to the given safety-level, see
+        the respective subclass’ :meth:`check_safety` implementation (or
+        documentation).
+        """
+
+        if isinstance(ast, str):
+            raise TypeError("First argument to compile_ast must be an ast")
+
+        self.check_safety(ast)
+
+        return compile(ast, filename, mode)
+
+class _Experimental(SafetyLevel):
+    """
+    The experimental safety level allows the following constructs:
+
+    * ``a`` is safe, if ``a`` does not start with an underscore
+    * String, integer and float literals are safe
+    * List, dict and tuple literals consisting of safe elements are
+      considered safe
+    * ``None`` is considered safe
+    * accessing attributes is considered safe, if the attribute name
+      does not start with an underscore
+
+    Let ``a``, ``b``, ``c`` and ``d`` be safe, then:
+
+    * ``a[b]``, ``a[b:c]``, ``a[b:c:d]`` (and any permutations) are
+      considered safe
+    * ``a X b`` with a binary operator ``X`` is considered safe
+    * ``X a`` with an unary operator ``X`` is considered safe
+    * ``a if b else c`` is considered safe
+    """
+
+    safety = -1
+
+    def _check_safety(self, node):
+        if node is None:
+            return True
+        elif isinstance(node, (ast.Expr)):
+            return self._check_safety(node.value)
+        elif isinstance(node, (ast.Name)):
+            if node.id.startswith("_"):
+                raise ValueError("Names starting with underscores are not"
+                                 " allowed")
+            return True
+        elif isinstance(node, (ast.Str, ast.Num)):
+            return True
+        elif isinstance(node, (ast.Tuple, ast.List)):
+            return all(map(self._check_safety, node.elts))
+        elif isinstance(node, (ast.Dict)):
+            return (all(map(self._check_safety, node.keys)) and
+                    all(map(self._check_safety, node.values)))
+        elif isinstance(node, (ast.Subscript)):
+            return (self._check_safety(node.slice) and
+                    self._check_safety(node.value))
+        elif isinstance(node, (ast.Slice)):
+            return (self._check_safety(node.lower) and
+                    self._check_safety(node.step) and
+                    self._check_safety(node.upper))
+        elif isinstance(node, (ast.Index)):
+            return self._check_safety(node.value)
+        elif isinstance(node, (ast.BinOp)):
+            return (self._check_safety(node.left) and
+                    self._check_safety(node.right))
+        elif isinstance(node, (ast.UnaryOp)):
+            return self._check_safety(node.operand)
+        elif isinstance(node, (ast.IfExp)):
+            return (self._check_safety(node.body) and
+                    self._check_safety(node.orelse) and
+                    self._check_safety(node.test))
+        else:
+            raise ValueError("Must not occur in {} expressions: {}".format(
+                self, node))
+
+    def __str__(self):
+        return "experimental"
+
+SafetyLevel.experimental = _Experimental()
+del _Experimental
+
+class _Conservative(SafetyLevel):
+    """
+    The conservative safety level allows literals, names and list/dict/tuple
+    compounds consisting of these.
+    """
+
+    safety = 0
+
+    def _check_safety(self, node):
+        if isinstance(node, ast.Name):
+            if node.id.startswith("_"):
+                raise ValueError("Names starting with underscores are not allowed"
+                                 " in conservative mode")
+            return True
+        elif isinstance(node, (ast.Str, ast.Num)):
+            return True
+        elif isinstance(node, (ast.Tuple, ast.List)):
+            return all(map(self._check_safety, node.elts))
+        elif isinstance(node, (ast.Dict)):
+            return (all(map(self._check_safety, node.keys)) and
+                    all(map(self._check_safety, node.values)))
+        elif isinstance(node, (ast.Expr)):
+            return self._check_safety(node.value)
+        else:
+            raise ValueError("Must not occur in {} expressions: {}".format(
+                self, node))
+
+    def __str__(self):
+        return "conservative"
+
+SafetyLevel.conservative = _Conservative()
+del _Conservative
+
+class _Unsafe(SafetyLevel):
+    """
+    This is **unsafe**. No checks are performed. Imports and everything are
+    allowed.
+    """
+
+    safety = -100
+
+    def _check_safety(self, node):
+        return True
+
+    def __str__(self):
+        return "unsafe"
+
+SafetyLevel.unsafe = _Unsafe()
+del _Unsafe
 
 class ForeachProcessor(TemplateProcessor):
     """
@@ -55,9 +257,9 @@ class ForeachProcessor(TemplateProcessor):
     xmlns = shared_ns
     namespaces = {"tea": str(xmlns)}
 
-    def __init__(self, allow_unsafe=False, **kwargs):
+    def __init__(self, safety_level=SafetyLevel.conservative, **kwargs):
         super().__init__(**kwargs)
-        self._allow_unsafe = allow_unsafe
+        self._safety_level = safety_level
         self.attrhooks = {}
         self.elemhooks = {
             (str(self.xmlns), "for-each"): [self.handle_foreach]}
@@ -96,10 +298,7 @@ def _():
                            "eval",
                            ast.PyCF_ONLY_AST).body
 
-        if not self._allow_unsafe:
-            if not isinstance(iter_ast, ast.Name):
-                raise ValueError("in safe mode, only one plain Name is allowed "
-                                 "in @tea:from")
+        self._safety_level.check_safety(iter_ast)
 
         loop.iter = iter_ast
         loop.target = bind_ast
